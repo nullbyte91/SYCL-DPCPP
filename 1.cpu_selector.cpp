5d561d120cf3f48680f0757169aa339f858f98e4
Jegathesan Shanmugam <nullbyte.in@gmail.com>
2022-01-11 11:52:05 +0530

    cpu selector and setup doc added

---
+++ 1.cpu_selector.cpp
@@ -0,0 +1,15 @@
+#include <CL/sycl.hpp>
+#include <iostream>
+using namespace sycl;
+
+int main() {
+  // Create queue to use the CPU device explicitly
+  queue Q{ cpu_selector{} };
+
+  std::cout << "Selected device: " <<
+    Q.get_device().get_info<info::device::name>() << "\n";
+  std::cout << " -> Device vendor: " <<
+    Q.get_device().get_info<info::device::vendor>() << "\n";
+
+  return 0;
+}
